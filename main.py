018af3aabe1a5e04abebd6a037820225d5c5e1fb
PythyaKa_ <41766735+Aditya23Scout@users.noreply.github.com>
2025-04-23 05:26:12 +0700

    Update main.py
    hapus flask

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-Thread(target=run).start()
-
-
 import os
 import math
 import zipfile
